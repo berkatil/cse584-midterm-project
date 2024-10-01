70ac524cdb77df7694e62a25e2861b4f4ee0db71
berkatil <atilberk98@gmail.com>
2024-10-01 11:25:39 -0400

    discard xis from xjs

--- dataset.py
+++ dataset.py
@@ -7,10 +7,15 @@ class ModelDetectionDataset(Dataset):
         self.concat = concat
 
     def __len__(self):
-        return len(self.data)
+        return 5#len(self.data)
     
     def __getitem__(self, idx):
         row = self.data.iloc[idx]
+        xi = row["xi"]
+        xj = row["xj"]
+        if xi in xj:
+            xj = xj.replace(xi, "")
         if self.concat:
-            return row["xi"] + row["xj"], "", row["label"]
-        return row["xi"], row["xj"], row["label"]
+            return xi + xj, "", row["label"]
+       
+        return xi, xj, row["label"]
